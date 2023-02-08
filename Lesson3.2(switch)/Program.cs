f038318f093fdee224bc6e49e7dcf18f7bfa9153
Viktor Shapoval <hastos30@gmail.com>
2023-02-08 21:10:14 +0200

    separated each application into a separate project

---
+++ Lesson3.2(switch)/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AuthenticationSwitch();
+    }
+
+    static void AuthenticationSwitch()
+    {
+        Console.WriteLine("Введите логин: ");
+        string login = Console.ReadLine();
+        string password;
+
+        switch (login)
+        {
+            case "USER":
+                Console.WriteLine("Введите пароль: ");
+                password = Console.ReadLine();
+
+                switch (password)
+                {
+                    case "KILLME":
+                        Console.WriteLine("\nHello User");
+                        break;
+                    default:
+                        Console.WriteLine("Password is not correct.");
+                        Console.WriteLine();
+                        AuthenticationSwitch();
+                        break;
+                }
+                break;
+
+            case "ADMIN":
+                Console.WriteLine("Введите пароль: ");
+                password = Console.ReadLine();
+
+                switch (password)
+                {
+                    case "KILLME":
+                        Console.WriteLine("\nHello Administrator");
+                        break;
+                    default:
+                        Console.WriteLine("Password is not correct.");
+                        Console.WriteLine();
+                        AuthenticationSwitch();
+                        break;
+                }
+                break;
+            case "":
+                Console.WriteLine("\nHello Anonimous");
+                break;
+
+            default:
+                Console.WriteLine("Login is not correct.");
+                Console.WriteLine();
+                AuthenticationSwitch();
+                break;
+        }
+    }
+}
