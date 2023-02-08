502c7617ff290a95ad018f6e7981a204a28c25e6
Viktor Shapoval <hastos30@gmail.com>
2023-02-08 21:54:51 +0200

    Task completed. Three programs in three projects

---
+++ Lesson3.1(if)/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AuthenticationIfElse();
+    }
+    static void AuthenticationIfElse()
+    {
+        Console.WriteLine("Введите логин: ");
+        string login = Console.ReadLine();
+
+        if (login == "USER")
+        {
+            Console.WriteLine("Введите пароль: ");
+            string password = Console.ReadLine();
+
+            if (password == "KILLME")
+            {
+                Console.WriteLine("\nHello User");
+            }
+            else
+            {
+                Console.WriteLine("Password is not correct.");
+                Console.WriteLine();
+                AuthenticationIfElse();
+            }
+        }
+        else if (login == "ADMIN")
+        {
+            Console.WriteLine("Введите пароль: ");
+            string password = Console.ReadLine();
+
+            if (password == "KILLME")
+            {
+                Console.WriteLine("\nHello Administrator");
+            }
+            else
+            {
+                Console.WriteLine("Password is not correct.");
+                Console.WriteLine();
+                AuthenticationIfElse();
+            }
+        }
+        else if (login == "")
+        {
+            Console.WriteLine("\nHello Anonimous");
+        }
+        else
+        {
+            Console.WriteLine("Login is not correct.");
+            Console.WriteLine();
+            AuthenticationIfElse();
+        }
+    }
+}
