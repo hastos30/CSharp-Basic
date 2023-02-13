6e16da81e974e3d2f0e331fe09c13ac9d96b15b4
Viktor Shapoval <hastos30@gmail.com>
2023-02-13 22:52:54 +0200

    complited task 5

---
+++ Lesson5/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        bool startStop = false;
+        do
+        {
+            Console.WriteLine("Введите нечетное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int center = number / 2;
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (i <= center)
+                    {
+                        if (j >= center - i && j <= center + i)
+                            Console.Write("*");
+                        else
+                            Console.Write(" ");
+                    }
+                    else
+                    {
+                        if (j >= center + i - number + 1 && j <= center - i + number - 1)
+                            Console.Write("*");
+                        else
+                            Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Хотите еще? 1 - да, 2 - нет");
+            int result = Convert.ToInt32(Console.ReadLine());
+            if(result == 1)
+               startStop = true;
+            else if(result == 2)
+                startStop = false;
+        }
+        while(startStop);
+        
+    }
+}
