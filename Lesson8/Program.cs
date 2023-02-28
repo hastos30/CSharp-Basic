3ae9981d37b0c3538b4d5775d8f5d926fe9b203d
Viktor Shapoval <hastos30@gmail.com>
2023-02-28 05:15:46 +0200

    Triangle in array task completed

---
+++ Lesson8/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        PrintTriangle();
+    }
+
+    static void PrintTriangle()
+    {
+        Console.Write("Введите количество строк, для обозначения велечины триугольника: ");
+        
+        int numberOfLine = int.Parse(Console.ReadLine());
+        int[][] triangle = TriangleСreationLogic(numberOfLine);
+
+        for (int i = 0; i < numberOfLine; i++)
+        {
+            for (int j = 0; j < 2 * numberOfLine - 1; j++)
+            {
+                if (triangle[i][j] == 1)
+                {
+                    Console.Write("*");
+                }
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int[][] TriangleСreationLogic(int numberOfLine)
+    {
+        int[][] triangle = new int[numberOfLine][];
+
+        for (int i = 0; i < numberOfLine; i++)
+        {
+            triangle[i] = new int[2 * numberOfLine - 1];
+
+            for (int j = 0; j < 2 * numberOfLine - 1; j++)
+            {
+                if (j >= numberOfLine - 1 - i && j <= numberOfLine - 1 + i)
+                {
+                    triangle[i][j] = 1;
+                }
+                else
+                {
+                    triangle[i][j] = 0;
+                }
+            }
+        }
+        return triangle;
+    }
+}
